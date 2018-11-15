0a7311371174639c7763d84003833269ad87bdf8
YuchenHu <yuchenhu@hsph.harvard.edu>
2018-11-15 12:16:19 -0500

    commit

---
+++ R/linreg.R
@@ -0,0 +1,15 @@
+linmodEst <- function(x, y) {
+  coef <- solve(t(x) %*% x) %*% t(x) %*% y
+  ## degrees of freedom and standard deviation of residuals
+  df <- nrow(x) - ncol(x)
+  sigma2 <- sum((y - x %*% coef) ^ 2) / df
+  ## compute sigma^2 * (x’x)^-1
+  vcov <- sigma2 * solve(t(x) %*% x)
+  colnames(vcov) <- rownames(vcov) <- colnames(x)
+  list(
+    coefficients = coef,
+    vcov = vcov,
+    sigma = sqrt(sigma2),
+    df = df
+  )
+}
